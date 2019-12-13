ec58e256d14cfcf6d1f88714f93e775cd51e551a
Anushri Mundada <anushri.mundada@charmhealth.com.au>
2019-12-13 17:53:35 +1000

    after adding the new chrome driver

---
+++ ExcelReader/Search.cs
@@ -0,0 +1,47 @@
+﻿using ExcelDataReader;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data;
+using System.IO;
+
+namespace SeleniumProject.ExcelReader
+{
+
+//    System.IO.FileNotFoundException
+//  HResult = 0x80070002
+//  Message=Could not find file 'C:\Users\anushri.mundada\source\repos\SeleniumProject\TestData\FlightSearch.xlsx;'.
+//  Source=<Cannot evaluate the exception source>
+//  StackTrace:
+//<Cannot evaluate the exception stack trace>
+
+
+    //1. create obj of filestream which points to excel file
+    //2. open excel using excelreaderfactory; it consiers sheets in the form of table
+    //3. use asdataset().tables[sheetname] that returns data in form of table
+    //4. use rows[rowindex][colindex] to read
+    [TestClass]
+    public class Search
+    {
+        [TestMethod]
+        public void TestReadExcel()
+        {
+            FileStream stream = new FileStream(@"C:\Users\anushri.mundada\source\repos\SeleniumProject\TestData\FlightSearch.xlsx", FileMode.Open, FileAccess.Read);
+            IExcelDataReader reader = ExcelReaderFactory.CreateOpenXmlReader(stream);//returns Iexceldata reader var is returned
+            DataTable table = reader.AsDataSet().Tables["Search"];
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                var origin_for = table.Rows[i];
+                for (int j = 0; j < origin_for.ItemArray.Length; j++)
+                {
+
+                    Console.WriteLine("Data : {0}", origin_for.ItemArray[j]);
+                }
+                Console.WriteLine();
+
+            }
+
+
+
+        }
+    }
+}
---
+++ ExcelReader/SearchUsingExcelReader.cs
@@ -0,0 +1,22 @@
+﻿using ExcelDataReader;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data;
+using System.IO;
+
+namespace SeleniumProject.ExcelReader
+{
+
+    [TestClass]
+    public class SearchUsingExcelReader
+    {
+        [TestMethod]
+        public void TestReadExcel()
+        {
+            string xlPath = @"C:\Users\anushri.mundada\source\repos\SeleniumProject\TestData\FlightSearch.xlsx";
+            Console.WriteLine(ExcelReader.GetCellData(xlPath, "Search", 2, 0));
+            Console.WriteLine(ExcelReader.GetCellData(xlPath, "Search", 2, 1));
+
+        }
+    }
+}
